fd6fd1e59afe71947e832cb8d2c27e36d01ec68e
mosa.abedelhady@mail.huji.ac.il <51644794+mosaabed@users.noreply.github.com>
2020-12-15 20:42:32 +0200

    finish matrix classes

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,10 +1,36 @@
 /* Write your code below */
-
+const matrix = require('./Matrix.js');
+console.log(matrix)
+class EmployeeMatrix extends matrix{
+    
+    loadData(data){
+        this.matrix =[]
+        for(let obj of data){
+            this.matrix.push(Object.values(obj))
+        }
+        this.rows = this.matrix.length
+        this.cols = this.matrix[0].length
+    }
+}
 
 
 
 //You can paste the code from the lesson below to test your solution
 
+
+let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
+
+// let m = new EmployeeMatrix()
+// console.log(m)
+// m.loadData(data)
+// console.log(m)
 
 
 
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,63 @@
 /* Write your code below */
+class Matrix{
+    constructor(rows , cols){
+        this.rows = rows 
+        this.cols = cols
+        this.matrix = this.generateMatrix(rows , cols)
+    }
 
+    generateMatrix(rows , cols){
+        let matrix = []
+        let temp = []
+        for (let i =0 ; i< rows ; i++){
+            temp = []
+            for (let j =0 ; j< cols ; j++){
+                temp.push(i*cols+j+1)
+            }
+            matrix.push(temp)
+        }
+        return matrix
+    }
+
+    findCoordinate(value){
+        for (let i =0 ; i< this.rows ; i++){
+            for (let j =0 ; j< this.cols ; j++){
+                if (this.matrix[i][j] == value){
+                    return {x: i , y :j}
+                }
+            }
+           
+        }
+    }
+
+}
 
 
 
 //You can paste the code from the lesson below to test your solution
 
+// let m = new Matrix(3, 4)
+// console.log(m)
+// m.print()
+// //prints
+// /*
+// 1       2       3       4
+// 5       6       7       8
+// 9       10      11      12
+// */
+
+// m.alter(0, 0, m.get(1, 1))
+// m.printColumn(0) //prints 6, 5, 9 (separate lines)
+// m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
+
+// console.log(m.findCoordinate(12)) //prints {x: 3, y: 2}
+// console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}
 
 
 
 /* Do not remove the exports below */
+
+
+
+
 module.exports = Matrix
